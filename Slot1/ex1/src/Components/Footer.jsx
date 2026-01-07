11f103f6e9d16114b63ba86e3fdb3db90e848482
rinndayy <nhi0372399699@gmail.com>
2026-01-07 14:24:23 +0700

    update2

--- Slot1/ex1/src/Components/Footer.jsx
+++ Slot1/ex1/src/Components/Footer.jsx
@@ -1,6 +1,10 @@
 function Footer() {
     return (
+        <>
         <h1>Đây là trang web của tôi</h1>
+        <p>Liên hệ: nhi0372399699@gmail.com</p>
+        </>
+        
     );
 }
 export default Footer;
