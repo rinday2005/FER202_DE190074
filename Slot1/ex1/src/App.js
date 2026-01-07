99d5744dbcd9801db7e77d0219e37db8a14c5eed
rinndayy <nhi0372399699@gmail.com>
2026-01-07 14:11:31 +0700

    update1

---
+++ Slot1/ex1/src/App.js
@@ -0,0 +1,11 @@
+import logo from './logo.svg';
+import './App.css';
+import Footer from './Components/Footer';
+
+function App() {   //function component
+  return (
+   <Footer />
+  );
+}
+
+export default App;
